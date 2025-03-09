59c10d5bc9ca3cc57da5824fea3ac9c07659942d
Ka Bun <bun2932005@gmail.com>
2025-03-09 13:57:59 +0700

    Unit 3

--- affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
+++ affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.affirmations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
@@ -20,7 +19,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
